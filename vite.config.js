b591dbffbd844b0bad8ecdd90df632819ac4134b
Geomar Ribeiro <geomar.ribeiro@example.com>
2026-04-18 22:54:56 -0300

    Libera vendas2.html nas configurações do Vite

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        vendas2: resolve(__dirname, 'vendas2.html'),
       },
     },
   },
